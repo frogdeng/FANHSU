3036728eab0f7ab2800cd6d659e2e5d88879e66b
albert deng <albertdeng@kkbox.com>
2019-05-16 18:41:18 +0800

    about search

--- public/js/for_isotope.js
+++ public/js/for_isotope.js
@@ -1,24 +1,42 @@
 $(document).ready(function(){
 
 	var qsRegex;
+	var buttonFilter;
+	var selectFilter;
 
+
+// init Isotope
 	var $grid = $('.grid').isotope({
 	  itemSelector: '.element_item',
 	  layoutMode: 'fitRows',
-	  filter: function() {
-	    return qsRegex ? $(this).text().match( qsRegex ) : true;
-	  }
+	  filter: function () {
+	    var $this = $(this);
+
+	    var searchResult = qsRegex ? $this.text().match(qsRegex) : true;
+	    // var buttonResult = buttonFilter ? $this.is( buttonFilter ) : true;
+	    // var selectResult = selectFilter ? $this.is( selectFilter ) : true;
+	    return searchResult;
+  		}
 	});
+
 
 
 	// bind filter button click
-	$('.filters-button-group').on( 'click', 'div', function() {
-	  var filterValue = $( this ).attr('data-filter');
+	$('#filters').on( 'click', 'div', function() {
+		buttonFilter = $(this).attr('data-filter');
 	  // use filterFn if matches value
 	  // filterValue = filterFns[ filterValue ] || filterValue;
-	  $grid.isotope({ filter: filterValue });
+
+	  // $grid.isotope();
+	  setCustomFilter();
 	});
 
+
+	// -- search --
+	// use value of search field to filter
+	var $quicksearch = $('#quicksearch').keyup(debounce(function () {
+	  setCustomFilter();
+	}));
 
 
 
@@ -33,48 +51,103 @@ $(document).ready(function(){
 
 
 
-	// $grid.isotope({ filter: '.filter__space'});
 
-	var strUrl = location.search;
-		 if (strUrl.indexOf("?") != -1) {
-	    var getSearch = strUrl.split("=");
-	    var thispage = getSearch[1]
+
+function setCustomFilter() {
+	var qsRegex = $('#quicksearch').val();
+	console.log("qsregex:" + qsRegex);
+	$('.element-item').removeClass('show');
+	// if( $('.element-item').text().match(qsRegex) ) {
+	//   $('.element-item').addClass('show');
+	// }
+	searchFilter = '';
+	$('.element-item').each(function (index) {
+	if (qsRegex == '') {
+	  $('.element-item').eq(index).addClass('show');
+	  searchFilter = '.show';
+	} else  if ( $(this).text().toLowerCase().indexOf(qsRegex) >= 0) {
+	  console.log($(this).text());
+	  $('.element-item').eq(index).addClass('show');
+	  searchFilter = '.show';
 	}
 
-	$grid.isotope({ filter: thispage});
-	// var getFilterValue = $('.filter_button_item').attr('data-filter');
+	});
 
-	// console.log(thispage )
-	// console.log(getFilterValue)
-	$('.button-group').find(thispage).addClass('is-checked');
+	console.log($('.show').length);
+	if (!buttonFilter) buttonFilter = '';
+
+	var selectFilters = [];
+	var filter = [];
+	if (selectFilters.length > 0) {
+	for (var selectFilter in selectFilters) {
+	  filter.push(selectFilters[selectFilter] + buttonFilter + searchFilter);
+	}
+	filter = filter.join(', ');
+	} else {
+	filter = buttonFilter + '.show';
+	}
+	console.log("filter:" + filter);
+	$grid.isotope({
+		filter: filter
+	});
+}
+
+
+
+// debounce so filtering doesn't happen every millisecond
+function debounce( fn, threshold ) {
+  var timeout;
+  return function debounced() {
+    if ( timeout ) {
+      clearTimeout( timeout );
+    }
+    function delayed() {
+      fn();
+      timeout = null;
+    }
+    timeout = setTimeout( delayed, threshold || 100 );
+  }
+}
 
 
 
 
 
 
-	// // -- search --
-	// // use value of search field to filter
-	// var $quicksearch = $('.quicksearch').keyup( debounce( function() {
-	//   	qsRegex = new RegExp( $quicksearch.val(), 'gi' );
-	//  	$grid.isotope();
-	// 	}, 200 ) );
 
 
-	// // debounce so filtering doesn't happen every millisecond
-	// function debounce( fn, threshold ) {
-	//   var timeout;
-	//   return function debounced() {
-	//     if ( timeout ) {
-	//       clearTimeout( timeout );
-	//     }
-	//     function delayed() {
-	//       fn();
-	//       timeout = null;
-	//     }
-	//     timeout = setTimeout( delayed, threshold || 100 );
-	//   }
+
+	// // change is-checked class on buttons
+	// $('.button-group').each( function( i, buttonGroup ) {
+	//   var $buttonGroup = $( buttonGroup );
+	//   $buttonGroup.on( 'click', 'div', function() {
+	//     $buttonGroup.find('.is-checked').removeClass('is-checked');
+	//     $( this ).addClass('is-checked');
+	//   });
+	// });
+
+
+
+	// // $grid.isotope({ filter: '.filter__space'});
+
+	// var strUrl = location.search;
+	// 	 if (strUrl.indexOf("?") != -1) {
+	//     var getSearch = strUrl.split("=");
+	//     var thispage = getSearch[1]
 	// }
+
+	// $grid.isotope({ filter: thispage});
+	// // var getFilterValue = $('.filter_button_item').attr('data-filter');
+
+	// // console.log(thispage )
+	// // console.log(getFilterValue)
+	// $('.button-group').find(thispage).addClass('is-checked');
+
+
+
+
+
+
 
 
 
